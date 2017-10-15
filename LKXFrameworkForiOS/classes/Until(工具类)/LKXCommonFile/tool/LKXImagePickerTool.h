8d360d7ff4742c0ba30061cf3e273df53356276f
l770826421 <770826421@qq.com>
2017-10-15 12:17:41 +0800

    1.增加了删除系统tabBar方法 2.增加了调用系统相册相机工具箱

---
+++ LKXFrameworkForiOS/classes/Until(工具类)/LKXCommonFile/tool/LKXImagePickerTool.h
@@ -0,0 +1,66 @@
+//
+//  LKXImagePickerTool.h
+//  malama_ca
+//
+//  Created by Developer on 15/4/15.
+//  Copyright (c) 2015年 Developer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@class LKXImagePickerTool;
+
+@protocol LKXImagePickerToolDelegate <NSObject>
+
+@optional
+/**
+ *  @author 刘克邪
+ *
+ *  @brief  选中相片或拍照后的代理
+ *
+ *  @param image 选中相片或拍照后的相片
+ */
+- (void)imagePickerToolDidSelectedImage:(UIImage *)image;
+
+@end
+
+/**
+ *  @author 刘克邪
+ *
+ *  @brief  调用手机相册或相机
+ */
+@interface LKXImagePickerTool : NSObject <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+{
+    UIImagePickerController *imagePickerVC;
+    BOOL isTakePicture;
+}
+
+
+/**
+ *  设置了delegate则BackPhotoBlock将会被注销,不能使用,二者选其一
+ */
+@property (nonatomic, weak) id<LKXImagePickerToolDelegate> delegate;
+
+@property (nonatomic, copy) void (^BackPhotoBlock)(UIImage *image);
+
+/**
+ * 初始化一个UIImagePickerController的工具类
+ * 只支持单拍和选择单张相片
+ * @prameter isEditing 单拍活着选择相片后是否编辑
+ */
+- (instancetype)initWithEditing:(BOOL)isEditing;
+
+/**
+ * 拍照
+ * @prameter dismissVC  推出UIImagePickerController的界面
+ */
+- (void)takePhotoWithVC:(UIViewController *)dismissVC;
+
+/** 
+ * 从相册中选择相片
+ * @prameter dismissVC  推出UIImagePickerController的界面
+ */
+- (void)selectOnePhotoWithVC:(UIViewController *)dismissVC;
+//- (void)selectMultiPhotoWithVC:(UIViewController *)delegateVC;
+
+@end
---
+++ LKXFrameworkForiOS/classes/Until(工具类)/LKXCommonFile/tool/LKXImagePickerTool.m
@@ -0,0 +1,141 @@
+//
+//  LKXImagePickerTool.m
+//  malama_ca
+//
+//  Created by Developer on 15/4/15.
+//  Copyright (c) 2015年 Developer. All rights reserved.
+//
+
+#import "LKXImagePickerTool.h"
+//#import "UIImage+Category.h"
+
+@implementation LKXImagePickerTool
+{
+    BOOL isTakePhoto;
+}
+
+
+- (instancetype)initWithEditing:(BOOL)isEditing
+{
+    self = [super init];
+    if (self)
+    {
+        if (imagePickerVC == nil)
+        {
+            imagePickerVC = [[UIImagePickerController alloc] init];
+            imagePickerVC.delegate = self;
+            imagePickerVC.allowsEditing = isEditing;
+        }
+    }
+    return self;
+}
+
+- (void)initializationImagePicker
+{
+    if (imagePickerVC == nil)
+    {
+        imagePickerVC = [[UIImagePickerController alloc] init];
+        imagePickerVC.delegate = self;
+//        imagePickerVC.allowsEditing = isEditing;
+    }
+}
+
+//拍照
+- (void)takePhotoWithVC:(UIViewController *)dismissVC
+{
+//    [self initializationImagePicker];
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera])
+    {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypeCamera;
+    }
+    else
+    {
+        [kMBHUDTool showHUDWithText:@"这设备不支持拍照功能" delay:2.0];
+        return;
+    }
+    
+    // 1.初始化捕捉设备(AVCaptureDevice),类型为AVMediaTypeVideo
+    AVCaptureDevice *captureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:captureDevice error:nil];
+    if (!input)
+    {
+        [kMBHUDTool showHUDWithText:@"您关闭了拍照功能,请到设置中去开启" delay:2.0];
+        return;
+    }
+    
+    isTakePhoto = YES;
+    [dismissVC presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+/**
+ * 从相册中选择相片
+ * @prameter dismissVC  推出UIImagePickerController的界面
+ */
+- (void)selectOnePhotoWithVC:(UIViewController *)dismissVC
+{
+//    [self initializationImagePicker];
+    if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypePhotoLibrary])
+    {
+        imagePickerVC.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    }
+    else
+    {
+        [kMBHUDTool showHUDWithText:@"本设备不支持相册功能" delay:2.0];
+        return;
+    }
+    isTakePhoto = NO;
+    [dismissVC presentViewController:imagePickerVC animated:YES completion:nil];
+}
+
+//保存到图片回调方法
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)err contextInfo:(void *)context
+{
+    if (err != nil)
+    {
+        NSLog(@"保存图片失败");
+    }
+    else
+    {
+        NSLog(@"保存图片成功");
+    }
+}
+
+#pragma mark - 
+#pragma mark ========================= UIImagePickerControllerDelegate ============
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    LKXMLog(@"info == %@", info);
+    UIImage *image;
+    if (picker.allowsEditing)
+    {
+        image = [info objectForKey:UIImagePickerControllerEditedImage];
+    }
+    else
+    {
+        image = [info objectForKey:UIImagePickerControllerOriginalImage];
+    }
+    
+    if (isTakePhoto)
+    {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+        });
+    }
+    
+    [picker dismissViewControllerAnimated:YES completion:^{
+        
+        if ([self.delegate respondsToSelector:@selector(imagePickerToolDidSelectedImage:)]) {
+            [self.delegate imagePickerToolDidSelectedImage:image];
+        } else if (self.BackPhotoBlock)
+        {
+            self.BackPhotoBlock(image);
+        }
+    }];
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+@end
