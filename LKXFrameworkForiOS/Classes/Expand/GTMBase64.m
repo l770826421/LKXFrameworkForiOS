4cd3decf2af23585b0551e33ec2fec923f7048d4
github_l770826421 <770826421@qq.com>
2022-09-04 16:20:43 +0800

    更换证书

--- LKXFrameworkForiOS/Classes/Expand/GTMBase64.m
+++ LKXFrameworkForiOS/Classes/Expand/GTMBase64.m
@@ -18,7 +18,8 @@
 
 #import "GTMBase64.h"
 #import "GTMDefines.h"
-#import "commoncrypto/commondigest.h"
+#import <CommonCrypto/CommonDigest.h>
+
 static const char *kBase64EncodeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 static const char *kWebSafeBase64EncodeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
 static const char kBase64PaddingChar = '=';
